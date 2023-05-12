5889413850d68446b007615f49ba3c4607ec746e
Adityamishra9719 <adityamishra971916@gmail.com>
2023-05-12 14:23:08 +0530

    rebase

---
+++ frontend/src/components/Services/weather.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+
+const Weather = () => {
+  return (
+    <Container sx={{ mt: { xs: 6, sm: 8 }, textAlign: 'center', padding: "5vmax" }}>
+     <Typography  variant="h4" color="primary" fontFamily="Roboto" fontWeight="bold" >
+        UNDER DEVLOPMENT
+      </Typography>
+    </Container>
+  );  
+};
+
+export default Weather;
