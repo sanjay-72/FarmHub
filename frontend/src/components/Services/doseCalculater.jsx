e182a7334c66d9b20230084de9252a470ad9a949
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-08-13 07:35:59 +0530

    Added calculation logic for Barley

--- frontend/src/components/Services/doseCalculater.jsx
+++ frontend/src/components/Services/doseCalculater.jsx
@@ -84,6 +84,82 @@ const Services = () => {
     setUserCropInfo({ ...userCropInfo, [e.target.name]: e.target.value });
   };
 
+  // Function for Dose calculation -> called when calculate button is clicked.
+
+  const updateAnswer = (e) => {
+    var acreArea = 0, answer = {};
+
+    // Converting area to Acres for easy calculation.
+    if (userCropInfo.unit === "HECTARE")
+      acreArea = userCropInfo.area * 2.47105;
+    else
+      acreArea = userCropInfo.area;
+
+
+    // Conditional implementation of calculation
+    if (userCropInfo.cropName === "Barley") {
+      //Values as per standards
+      let lBound = ((5.055 - 1) * acreArea).toFixed(2);
+      let uBound = ((5.055 + 1) * acreArea).toFixed(2);
+      answer.manures = {};
+      answer.manures.FYM = `${lBound} - ${uBound} t`;
+      answer.manures.urea = `${(55 * acreArea).toFixed(2)} Kg`;
+      answer.seedRate = `43.54 - 54.43 Kg/Acre`;
+      answer.seedsRequired = `${(43.54 * acreArea).toFixed(2)} - ${(54.43 * acreArea).toFixed(2)} Kgs`;
+      answer.manures.phosphate = `${(12 * acreArea).toFixed(2)} Kg`;
+      answer.manures.pottasium = `${(6 * acreArea).toFixed(2)} Kg`;
+    }
+
+    else if (userCropInfo.cropName === "Brinjal") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Carrot") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Cauliflower") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Chickpea") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Mustard") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Onion") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Potato") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Radish") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Rice") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    else if (userCropInfo.cropName === "Tomato") {
+      // answer.FYM = 15 * acreArea;
+    }
+
+    //If nothing above matched then default will be as same as Wheat
+    else {
+      answer.FYM = 15 * acreArea;
+    }
+
+
+    console.log(answer);
+  }
+  //Logic for dose calculation ends.
+
   return (
     <Container sx={{ mt: { xs: 6, sm: 8 }, padding: "5vmax" }}>
       <Typography
@@ -200,6 +276,7 @@ const Services = () => {
             sx={{ marginTop: "1rem", width: "80%", textTransform: "none" }}
             color="tertiary"
             variant="contained"
+            onClick={updateAnswer}
           >
             Calculate
           </Button>
@@ -207,6 +284,7 @@ const Services = () => {
 
         <Grid item xs={6} display="block">
           <Typography
+            id="dose-calculator-output"
             paddingTop={25}
             align="center"
             variant="h5"
